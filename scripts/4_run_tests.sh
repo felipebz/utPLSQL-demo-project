b46d40c42eb8a351e263d1167009cfbdaac7e6de
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-05-22 22:41:44 -0300

    feat: Remove unused reporters

--- scripts/4_run_tests.sh
+++ scripts/4_run_tests.sh
@@ -10,6 +10,4 @@ utPLSQL-cli/bin/utplsql run ut3_demo/ut3_demo@//127.0.0.1:1521/XE?oracle.net.dis
   -f=ut_documentation_reporter  -c \
   -f=ut_coverage_sonar_reporter     -o=coverage.xml \
   -f=ut_sonar_test_reporter         -o=test_results.xml \
-  -f=ut_coverage_cobertura_reporter -o=cobertura.xml \
-  -f=ut_junit_reporter              -o=junit_test_results.xml \
   --failure-exit-code=0
