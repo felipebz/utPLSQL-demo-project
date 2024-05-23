e45b759b379f519ddcdff5e7a10ab57da01c9b3e
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-05-22 22:43:55 -0300

    feat: Remove the source mapping config from utplsql-cli and use the new utPLSQL Coverage Report Importer from ZPA 3.5.0-SNAPSHOT

--- scripts/4_run_tests.sh
+++ scripts/4_run_tests.sh
@@ -3,10 +3,6 @@
 set -ev
 
 utPLSQL-cli/bin/utplsql run ut3_demo/ut3_demo@//127.0.0.1:1521/XE?oracle.net.disableOob=true \
-  -source_path=source -regex_expression="(\w+)/(\w+)/(R__)(\w+)\.(\w+)\.(\w+)$" \
-  -owner_subexpression=4 \
-  -name_subexpression=5 \
-  -type_subexpression=6 \
   -f=ut_documentation_reporter  -c \
   -f=ut_coverage_sonar_reporter     -o=coverage.xml \
   -f=ut_sonar_test_reporter         -o=test_results.xml \
