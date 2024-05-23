554d23ebd07df032ce6cd5c1848b15413116ea3e
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-05-23 13:21:04 -0300

    feat: Rename all PL/SQL files to .sql

--- source/install.sql
+++ source/install.sql
@@ -1,6 +1,6 @@
 @source/award_bonus/V1.01__employees_test.sql
-@source/award_bonus/R__ut3_demo.award_bonus.prc
-@source/between_string/R__ut3_demo.betwnstr.fnc
+@source/award_bonus/award_bonus.sql
+@source/between_string/betwnstr.sql
 @source/remove_rooms_by_name/V1.02__rooms.sql
-@source/remove_rooms_by_name/R__ut3_demo.remove_rooms_by_name.prc
+@source/remove_rooms_by_name/remove_rooms_by_name.sql
 exit
